162ad79b10623c7d6e8eae82084aacdca19caadd
umutcanaydin69 <>
2026-08-17 12:01:04 +0000

    Table Modification

---
+++ ddl_and_dml_in_sql/table_modification/query.sql
@@ -0,0 +1,12 @@
+ALTER TABLE employees ADD country VARCHAR(50);
+
+INSERT INTO employees (employee_id, first_name, last_name, department, salary, country) VALUES
+    (1, 'Emily', 'Torres', 'Operations', 80000, 'United Kingdom'),
+    (2, 'David', 'Bobr', 'Engineering', 95000, 'Poland');
+
+
+SELECT * FROM employees;
+
+TRUNCATE TABLE employees;
+
+ALTER TABLE employees DROP COLUMN country;
